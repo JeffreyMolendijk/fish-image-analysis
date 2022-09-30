d79feaa93d354ed92f83e0bc3df48a1d7e97a235
Jeffrey Molendijk <jeffreymolendijk@live.com>
2022-10-01 01:31:57 +1000

    Added github actions

--- ml.py
+++ ml.py
@@ -1,20 +1,3 @@
-# https://stackabuse.com/image-recognition-in-python-with-tensorflow-and-keras/
-# https://www.geeksforgeeks.org/python-image-classification-using-keras/
-
-
-# explain ML
-# https://www.analyticsvidhya.com/blog/2020/03/6-python-libraries-interpret-machine-learning-models/
-
-# Use mahotas to detect features from templates (e.g. head, tail)?
-
-# Use templates to get features / crops?
-# https://www.geeksforgeeks.org/template-matching-using-opencv-in-python/
-
-
-# This one seems promising
-# https://datascience.stackexchange.com/questions/63434/unsupervised-image-classification
-
-# Imports
 from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
@@ -50,20 +33,16 @@ for i, imagepath in enumerate(filelist):
 # Clustering
 kmeans = KMeans(n_clusters=number_clusters, random_state=0).fit(np.array(featurelist))
 
-
 kmeans_result = pd.DataFrame()
 kmeans_result['files'] = filelist
-kmeans_result['lables'] = kmeans.labels_
+kmeans_result['labels'] = kmeans.labels_
 
 kmeans_result.to_excel(os.path.join('input', 'cluster', 'clusters.xlsx'))
 
-# Copy images renamed by cluster 
-# Check if target dir exists
 try:
     os.makedirs(targetdir)
 except OSError:
     pass
-# Copy with cluster name
 print("\n")
 for i, m in enumerate(kmeans.labels_):
     try:
@@ -72,8 +51,5 @@ for i, m in enumerate(kmeans.labels_):
     except:
         continue
 
-
 # TODO: Flip crops into the same orientation... How to determine if fish is left/right facing?
-# Potentially using a histogram of non-zero values in thresh?
-
 # TODO: Only run script on magenta > Just comparing bones? > This also ignores black crops
